20779364ac2c641ab2f60cd3fe60ecbc642a1be4
Mingon_Choe <skandy57@naver.com>
2022-09-21 18:04:38 +0900

    거래내역보는기능 뜯어고침, 이미지 크기 조절할 필요 있음

---
+++ springTeamProject/src/main/java/sol/one/VO/TradelistVO.java
@@ -0,0 +1,26 @@
+package sol.one.VO;
+
+import lombok.Data;
+
+@Data
+public class TradelistVO {
+
+	private int product_id;
+	
+	private int pd_status;
+	
+	private String pd_name;
+	
+	private String trade_time;
+
+	private String pd_price;
+
+	private String pd_price0;
+
+	private String pd_img;
+	
+	private int pd_discount;
+
+	private String pd_date;
+
+}
